0ce55bd1201d6702a0d39ab6fdd528e8dec89b19
jishaoming <jishaoming@jishaomingdeMacBook-Pro.local>
2015-09-06 22:07:29 +0800

    26 int[] to vector
    26 int[] to vector

--- 26.cpp
+++ 26.cpp
@@ -1,17 +1,24 @@
 //26 Remove Duplicates from Sorted Array
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-      int removeDuplicates(int A[], int n) {
-          if (n == 0) return 0;
-          int index = 0;
-          for (int i = 1; i < n; i++) {
-              if (A[index] != A[i])
-                  A[++index] = A[i];
-          }
-          return index + 1;
+	int removeDuplicates(vector<int>& nums) {
+      	int n = nums.size();
+
+      	if (n == 0) 
+      		return 0;
+
+      	int index = 0;
+
+      	for (int i = 1; i < n; i++) 
+      	{
+      		if (nums[index] != nums[i])
+      			nums[++index] = nums[i];
+      	}
+      	return index + 1;
       }
 };
 
@@ -19,7 +26,7 @@ int main(int argc, char const *argv[])
 {
 	Solution s;
 	int A[] = {1 , 1 , 2};
-	int n = 3;
-	cout << s.removeDuplicates(A,n);
+	vector<int> v(A,A+3);
+	cout << s.removeDuplicates(v);
 	return 0;
 }
